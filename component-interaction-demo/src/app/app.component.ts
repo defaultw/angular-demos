2f8cfa5ab9c37a5916163dedcd72a572a4bc341a
Default.W <xiaojiangzt@gmail.com>
2019-12-11 00:05:20 +0800

    add ngOnchange simple

--- component-interaction-demo/src/app/app.component.ts
+++ component-interaction-demo/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import {
+    ChildNgonchangeComponentComponent
+} from './child-ngonchange-component/child-ngonchange-component.component';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   * 属性装饰器，用于配置一个视图查询。
+   * 变更检测器会在视图的 DOM 中查找能匹配上该选择器的第一个元素或指令.
+   * 如果视图的 DOM 发生了变化，出现了匹配该选择器的新的子节点，该属性就会被更新。
+   *
+   * 此处用在 通过 ngOnChanges()截听属性值的变化
+   */
+  @ViewChild(ChildNgonchangeComponentComponent, { static: false })
+  onChangeChild: ChildNgonchangeComponentComponent;
+
   title = 'component-interaction-demo';
 
   // 父组件通过 @Input 传值给子组件
@@ -13,4 +27,14 @@ export class AppComponent {
 
   // 通过 setter 截听输入属性值的变化
   public appSetterData = ['work', '', 'sleep', '', 'drink'];
+
+  // 通过 ngOnChanges()截听属性值的变化
+  public appInputValue: string;
+
+  resetInputValue() {
+    this.appInputValue = 'init';
+    if (this.onChangeChild) {
+      this.onChangeChild.clearLog();
+    }
+  }
 }
--- component-interaction-demo/src/app/child-ngonchange-component/child-ngonchange-component.component.ts
+++ component-interaction-demo/src/app/child-ngonchange-component/child-ngonchange-component.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child-ngonchange-component',
   templateUrl: './child-ngonchange-component.component.html',
   styleUrls: ['./child-ngonchange-component.component.css']
 })
-export class ChildNgonchangeComponentComponent implements OnInit {
+export class ChildNgonchangeComponentComponent implements OnInit, OnChanges {
+  @Input() inputValue: string;
 
-  constructor() { }
+  // 存放修改log
+  public changeLog: string[] = [];
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    for (const proName in changes) {
+      if (changes.hasOwnProperty(proName)) {
+        // 此处需要判断对象中是否存在key
+        const chng = changes[proName];
+        const cur = JSON.stringify(chng.currentValue);
+        const prev = JSON.stringify(chng.previousValue);
+        this.changeLog.push(`${proName}已修改,修改后:${cur},修改前:${prev};`);
+      }
+    }
   }
 
+  clearLog() {
+    this.changeLog = [];
+  }
+
+  ngOnInit() {}
 }
